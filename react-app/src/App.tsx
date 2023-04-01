81a7cabb8779333986252cf963223414bbb03ccc
alireza-mehrzad <alireza.mehrzad369@gmail.com>
2023-04-01 12:38:44 -0700

    btn add to app

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,8 +1,10 @@
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 function App() {
   return (
     <div>
       <Alert text="ve to chi" />
+      <Button text="click me" />
     </div>
   );
 }
